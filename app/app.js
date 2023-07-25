527bca5cc908198a52a0a12105a00a802ec66bc9
MaiVanThinhCN19A <92565482+MaiVanThinh1@users.noreply.github.com>
2023-07-25 17:24:24 +0700

    update

---
+++ app/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/register", (req, res, next) => {
+  res.status(200).json({ message: "Service is up" });
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+      status: error.status,
+    },
+  });
+});
+
+module.exports = app;
